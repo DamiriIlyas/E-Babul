cb72d0e87dc313da7226776415db47cbc634c5ca
Damiri <ilyasdamiri25@gmail.com>
2023-06-27 13:12:08 +0700

    update

---
+++ database/migrations/2023_06_24_191133_create_formulirs_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('formulirs', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_lengkap');
+            $table->string('nisn');
+            $table->string('jenis_kelamin');
+            $table->string('ttl');
+            $table->string('alamat');
+            $table->string('asal_sekolah');
+            $table->string('tahun_lulus');
+            $table->string('nama_wali');
+            $table->string('nik');
+            $table->string('pekerjaan_wali');
+            $table->string('alamat_wali');
+            $table->string('nomor_wa');
+            $table->string('pilihan_sekolah');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('formulirs');
+    }
+};
