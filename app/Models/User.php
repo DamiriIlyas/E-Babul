19bcb9ec5eb971494b236490ebc37ecb5d2cc29c
Damiri <ilyasdamiri25@gmail.com>
2023-07-02 15:57:50 +0700

    update

--- app/Models/User.php
+++ app/Models/User.php
@@ -39,7 +39,7 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function formulir()
+    public function formulirs()
     {
         return $this->hasOne(Formulir::class);
     }
