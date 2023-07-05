f86af818d41fa93cd744bcb4d964f7b98ea42e40
Damiri <ilyasdamiri25@gmail.com>
2023-07-05 16:53:01 +0700

    PUSH

--- app/Http/Controllers/API/ImageController.php
+++ app/Http/Controllers/API/ImageController.php
@@ -26,24 +26,24 @@ class ImageController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
-    {
-        $validator = Validator::make($request->all(), [
-            'image' => 'max:10000|mimes:jpg.jpeg.png.gif',
-        ]);
+{
+    $validator = Validator::make($request->all(), [
+        'image' => 'max:10000|mimes:jpg,jpeg,png,gif',
+    ]);
 
-        if ($validator->fails()) {
-            return response()->json(['error' => $validator->errors()->getMessages()], 400);
-        }
-
-        if ($request->hasFile('image')) {
-            $image = $request->file('image');
-            $file_path = $image->store('public/image');
-            $url = Storage::url($file_path);
-            return url($url);
-        }
-
-        return '';
+    if ($validator->fails()) {
+        return response()->json(['error' => $validator->errors()->getMessages()], 400);
     }
+
+    if ($request->hasFile('image')) {
+        $image = $request->file('image');
+        $file_path = $image->store('public/image');
+        $url = Storage::url($file_path);
+        return url($url);
+    }
+
+    return '';
+}
 
     /**
      * Display the specified resource.
