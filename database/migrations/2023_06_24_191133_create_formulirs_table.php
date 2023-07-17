22a548642133bdb97b10b64040e4270aa5f7b9c2
Damiri <ilyasdamiri25@gmail.com>
2023-07-17 14:39:26 +0700

    update

--- database/migrations/2023_06_24_191133_create_formulirs_table.php
+++ database/migrations/2023_06_24_191133_create_formulirs_table.php
@@ -33,6 +33,7 @@ return new class extends Migration
             $table->string('foto');
             $table->foreignId('user_id')->constrained('users');
             $table->timestamps();
+            
         });
     }
 
